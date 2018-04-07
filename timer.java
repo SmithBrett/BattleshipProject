fe88d2d6cc93a70e91784b5b81e914b4dc4d6089
SmithBrett <38050124+SmithBrett@users.noreply.github.com>
2018-04-07 14:58:04 -0400

    Added timer class

---
+++ timer.java
@@ -0,0 +1,62 @@
+
+import java.util.concurrent.*;
+import static java.util.concurrent.TimeUnit.*;
+
+/*
+ * Name: timer.java
+ * Author: Brett Smith
+ * Ver: 1.0
+ * Date: 4/7/2018
+ * 
+ * This is a simple program that handle the timer functionality.
+ */
+
+public class timer
+{
+	private ScheduledThreadPoolExecutor timerThread;
+	private ScheduledFuture<?> timerDecHandle;
+	
+	private volatile int count;
+	private volatile boolean flag;
+	
+	//getters/setters
+	public void setCount(int count)
+	{
+		this.count=count;
+	}
+	public int getCount()
+	{
+		return count;
+	}
+	public void setFlag(boolean flag)
+	{
+		this.flag=flag;
+	}
+	public boolean getFlag()
+	{
+		return flag;
+	}
+	
+	//initializes timer
+	public void initTimer()
+	{
+		timerThread = new ScheduledThreadPoolExecutor(1);
+		timerThread.setRemoveOnCancelPolicy(true);
+		timerThread.setExecuteExistingDelayedTasksAfterShutdownPolicy(false);
+	}
+	//starts count down timer
+	public void startTimer(int sec)
+	{
+		count=sec;
+		Runnable timeDec = new Runnable(){public void run(){count--; /*System.out.println(count);*/}};
+		timerDecHandle = timerThread.scheduleAtFixedRate(timeDec,5,1,SECONDS);
+		timerThread.schedule(new Runnable(){public void run() {timerDecHandle.cancel(true);flag=true;}},sec+5-1,SECONDS);
+		
+	}
+	//stops timer
+	public void stopTimer()
+	{
+		timerDecHandle.cancel(true);
+		timerThread.shutdownNow();
+	}
+}

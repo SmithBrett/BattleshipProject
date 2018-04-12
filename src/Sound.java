67283f303d2cdd454a74222fe1ff8d411cebd16e
adamdingess <38050223+adamdingess@users.noreply.github.com>
2018-04-11 22:47:13 -0400

    Updated Sound Class
    Added muting and menu music endless looping.

--- src/Sound.java
+++ src/Sound.java
@@ -2,12 +2,10 @@ import java.util.*;
 import java.io.*;
 import javax.sound.sampled.*;
 
-/*Adam Dingess - Ver: 0.1 - 4/8/18
+/*Adam Dingess - Ver: 0.2 - 4/11/18
  * 
  * To play a sound, you need to invoke "menuSound();", "shotSound();", "timerSound();"... etc. 
  * You will also need to change the location of the audio files for each to test this. 
- * 
- * I'm still working on muting the sound 
  * 
  * If you want to try to find sounds: they must be .wav (they can be converted here: https://audio.online-convert.com/convert-to-wav)
  * Additionally, they must all be: 16-bit resolution, 44,100 Hz sampling rate, and mono audio channel
@@ -16,10 +14,9 @@ import javax.sound.sampled.*;
 
 public class Sound {
 
-	   static Scanner console = new Scanner(System.in);
-	   //C:/Users/Adam/workspace/SoundforBS/src/menu.wav
-	//   String filePath = fileLoc();
+	   
 	 
+	   private static boolean mute = false;
 	   private static Clip clip;
 	   
 	  public static void menuSound() {
@@ -34,7 +31,10 @@ public class Sound {
 		         e.printStackTrace();
 		      } catch (LineUnavailableException e) {
 		         e.printStackTrace();
-		      } clip.start();
+		      } while (mute != true) 
+		     	{ clip.setLoopPoints(0,-1);
+			      clip.loop(50);  break; }
+		      
 		   }
 	  
 	  public static void shotSound() {
@@ -49,7 +49,9 @@ public class Sound {
 	         e.printStackTrace();
 	      } catch (LineUnavailableException e) {
 	         e.printStackTrace();
-	      } clip.start();
+	      } 
+	      while (mute != true) 
+	     	{clip.start(); break;}
 	   } 
 	  
 	  public static void timerSound() {
@@ -65,8 +67,8 @@ public class Sound {
 	      } catch (LineUnavailableException e) {
 	         e.printStackTrace();
 	      } 
-	      clip.start();
-	      
+	      while (mute != true) 
+	     	{clip.start(); break;}
 	   }
 	  
 	  public static void turnSwitchSound() {
@@ -81,12 +83,54 @@ public class Sound {
 	         e.printStackTrace();
 	      } catch (LineUnavailableException e) {
 	         e.printStackTrace();
-	      } clip.start();
+	      } while (mute != true) 
+	     	{clip.start(); break;}
 	   }
 	  
+	  public static void hitSound() {
+	      try {
+	    	 File audiofile= new File("C:/Users/Adam/workspace/SoundforBS/src/hitSound.wav");	     
+	         AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(audiofile);
+	         clip = AudioSystem.getClip();
+	         clip.open(audioInputStream);
+	      } catch (UnsupportedAudioFileException e) {
+	         e.printStackTrace();
+	      } catch (IOException e) {
+	         e.printStackTrace();
+	      } catch (LineUnavailableException e) {
+	         e.printStackTrace();
+	      } 
+	      while (mute != true) 
+	     	{clip.start(); break;}
+	   } 
+	  
+	  public static void missSound() {
+	      try {
+	    	 File audiofile= new File("C:/Users/Adam/workspace/SoundforBS/src/missSound.wav");	     
+	         AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(audiofile);
+	         clip = AudioSystem.getClip();
+	         clip.open(audioInputStream);
+	      } catch (UnsupportedAudioFileException e) {
+	         e.printStackTrace();
+	      } catch (IOException e) {
+	         e.printStackTrace();
+	      } catch (LineUnavailableException e) {
+	         e.printStackTrace();
+	      } 
+	      while (mute != true) 
+	     	{clip.start(); break;}
+	   } 
+	  
 	   public static void mute() {
-		   clip.stop();
+		   if(mute == false)
+		   {mute = true;
+		   if (clip.isRunning()){clip.stop();}
+		   }
+		   
+		   else mute = false;
+		   
 	   }
+	   
 	  
 }	
 
